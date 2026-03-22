5e83ea34975f9ea280ba3faf39fd6744f0efd9e3
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-22 21:02:54 +0300

    feat(types): add ProjectConfig, Dependency, Framework and Language types

---
+++ src/types/type.ts
@@ -0,0 +1,24 @@
+export type Framework = 'react' | 'nextjs' | 'vue' | 'nuxt'
+export type Language  = 'typescript' | 'javascript'
+export type Styling   = 'tailwind' | 'scss' | 'css-modules' | 'styled-components'
+export type Manager   = 'npm' | 'yarn' | 'pnpm'
+
+export interface Dependency {
+  id: string
+  name: string
+  version: string
+  description: string
+  category: 'ui' | 'state' | 'routing' | 'fetching' | 'testing' | 'utils'
+  devOnly?: boolean
+  incompatibleWith?: string[]
+  requiredWith?: string[]
+}
+
+export interface ProjectConfig {
+  name: string
+  framework: Framework
+  language: Language
+  styling: Styling
+  packageManager: Manager
+  dependencies: Dependency[]
+}
