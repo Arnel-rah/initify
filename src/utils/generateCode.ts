ac2d889409fec3b28227cff179eb883d59c3c58b
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-03-22 21:06:26 +0300

    feat(generator): add buildPackageJson from ProjectConfig

---
+++ src/utils/generateCode.ts
@@ -0,0 +1,23 @@
+import type { ProjectConfig } from '../types/type'
+
+export function buildPackageJson(config: ProjectConfig): string {
+  // TODO: construire l'objet JSON selon les deps choisies
+  const pkg = {
+    name: config.name,
+    version: '0.0.1',
+    scripts: { /* selon framework */ },
+    dependencies: {},
+    devDependencies: {},
+  }
+  return JSON.stringify(pkg, null, 2)
+}
+
+export function buildReadme(config: ProjectConfig): string {
+  // TODO: générer un README avec les instructions d'install
+  return `# ${config.name}`
+}
+
+export async function downloadZip(config: ProjectConfig) {
+  // TODO: utiliser la lib JSZip pour zipper tous les fichiers générés
+  // npm install jszip
+}
